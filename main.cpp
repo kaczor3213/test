52c2e4db4f2034558de9f48854e3deebc5dff3f7
Wojtas059 <wojtas.maj34@gmail.com>
2018-11-23 15:42:00 +0100

    Dodalem sume dwoch liczb

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
 int main() {
+	int i=8;
+	int w=5;
+	int n;
 	std::cout << "Hello world!\n";
+	n=w+i;
+	std::cout << "Suma liczb: " << w << " i " << i << " ,wynosi: " << n;
+	
+	
 	return 0;
 }
