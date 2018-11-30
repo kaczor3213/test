5de0b766fe47157706ad1ca2df01bfa006a434c9
kaczor3213 <kacz.pacz@gmail.com>
2018-11-30 15:04:09 +0100

    clean up

--- main.cpp
+++ main.cpp
@@ -1,51 +1,6 @@
 #include <iostream>
-<<<<<<< HEAD
- 
-	int main(){
-		std::cout<<"Hello world! \n"<<"Kaczor_3213 \n";
-	}
-=======
-int main() {
-<<<<<<< HEAD
-	int i=8;
-	int w=5;
-	int n;
-	std::cout << "Hello world!\n";
-<<<<<<< HEAD
-<<<<<<< HEAD
-	std::cout << "Greetings from Maciek!\n";
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-	
-	
-	std::cout << "Tu Michal!\n" ;
-	
-=======
->>>>>>> d00e08172d2f936d21a30d9bdf328cf7cc060ae0
-	std::cout << "Jestem glodny\n";
->>>>>>> 54c91a95d894b5e16778b867397c139350093525
-=======
-<<<<<<< HEAD
-	for(int i=0;i<50;i++)
-		cout<<"Blabla haha\n";
-	
-=======
-	n=w+i;
-	std::cout << "Suma liczb: " << w << " i " << i << " ,wynosi: " << n;
-	
-	
-=======
+
+int main(){	
 	std::cout << "Hello world!\n"
-		<< "Witaj grupo!\n";
->>>>>>> 0b03ad973dd5638ce3914f0dc0900228ebb04d09
->>>>>>> f76848c64f389feaa5ab581fb28a4bc445c6f0ee
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/master
-=======
->>>>>>> dd5835a261735c9b66bb837a47736a28bd8831fd
->>>>>>> 54c91a95d894b5e16778b867397c139350093525
 	return 0;
 }
->>>>>>> refs/remotes/origin/master
